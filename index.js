8e015059dbd7836c0109856179aedac91f70377b
Sudipto Ghosh <sudiptoghosh@in.ibm.com>
2019-10-18 10:41:08 +0000

    New message - Friday 18th

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! from SG Demo App in Dallas Server");
+    response.end("Hello World! from SG NODEJS App on Friday 18th");
 
 });
 
